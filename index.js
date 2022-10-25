bfe3f88686d84f52748009fdb3a7d64bb30f17d0
Maxwell Appiah Gyimah <maxappiahofficial@gmail.com>
2022-10-25 23:06:09 +0000

    map results

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const { parse } = require('csv-parse')
-const fs = require('fs');
+const { parse } = require("csv-parse");
+const fs = require("fs");
 
-const results = [];
+const habitablePlanet = [];
 
-fs.createReadStream('kepler_data.csv')
-.pipe(parse())
-.on('data', (data) => {
-    results.push(data);
-})
-.on('error', (err) => {
+function isHabitablePlanet (planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+  && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+  && planet['koi_prad'] < 1.6;
+}
+
+fs.createReadStream("kepler_data.csv")
+  .pipe(parse({
+    comment: '#',
+    columns: true,
+  }))
+  .on("data", (data) => {
+    if(isHabitablePlanet(data)) {
+      habitablePlanet.push(data);
+    }
+  })
+  .on("error", (err) => {
     console.log(err);
-})
-.on('end', () => {
-    console.log(results)
-    console.log('all done');
-});
+  })
+  .on("end", () => {
+    console.log(habitablePlanet.map((planet) => planet['kepler_name']
+  ));
+    console.log(`${habitablePlanet.length} habitable planets found`);
+    console.log("all done");
+  });
