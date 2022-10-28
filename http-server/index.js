96adf4551babb63880482543ddc4a9eaf29d82ca
Maxwell Appiah Gyimah <maxappiahofficial@gmail.com>
2022-10-28 03:43:21 +0000

    create server and static response

---
+++ http-server/index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        // 'Content-Type': 'text/plain',
+        'Content-Type': 'application/json',
+    });
+    
+    // res.end('Hello! Sir Isaac Newton is your friend!');
+    res.end(JSON.stringify({
+        id: 1,
+        name: 'Sir Isaac Newton',
+    }))
+});
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+});
