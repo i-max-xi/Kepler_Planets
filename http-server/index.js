31b09a50975884cc74cbbdb9a387ca2f0ee22849
Maxwell Appiah Gyimah <maxappiahofficial@gmail.com>
2022-10-30 23:59:54 +0000

    POST

--- http-server/index.js
+++ http-server/index.js
@@ -1,20 +1,65 @@
-const http = require('http');
+const http = require("http");
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer();
+
+const friends = [
+  {
+    id: 0,
+    name: "Nikola Tesla",
+  },
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
+  },
+  {
+    id: 2,
+    name: "Albert Einstein",
+  },
+];
+
+server.on("request", (req, res) => {
+  const items = req.url.split("/");
+
+  if(req.method === 'POST' && items[1] === 'friends'){
+    req.on('data', (data) => {
+      const friend = data.toString();
+      console.log(`Request:`, friend);
+      friends.push(JSON.parse(friend));
+      req.pipe(res);
+    })
+  }
+
+  else if (req.method === 'GET' && items[1] === "friends") {
     res.writeHead(200, {
-        // 'Content-Type': 'text/plain',
-        'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     });
-    
-    // res.end('Hello! Sir Isaac Newton is your friend!');
-    res.end(JSON.stringify({
-        id: 1,
-        name: 'Sir Isaac Newton',
-    }))
+
+    if (items.length === 3) {
+      const friendIndex = parseInt(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]))
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (items[1] === "messages") {
+    res.setHeader("Content-Type", "text/html");
+
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<ul>");
+    res.write("<li>Hello Isaac</li>");
+    res.write("<li>What are your thoughts on Astronomy</li>");
+    res.write("</ul>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  } else {
+    res.statusCode = 400;
+    res.end("404 NOT FOUND !");
+  }
 });
 
 server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`)
+  console.log(`Listening on port ${PORT}...`);
 });
